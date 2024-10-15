2c6bba07b5aa5fa09834e10998e3c8515cd66fd9
Berryzin625 <88551634+Berryzin625@users.noreply.github.com>
2024-10-15 11:37:10 -0300

    Update add-machine.ts

--- pages/api/add-machine.ts
+++ pages/api/add-machine.ts
@@ -1,11 +1,12 @@
 // pages/api/add-machine.ts
-import connectToDatabase from '../../lib/dbConnect';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { connectToDatabase } from '../../lib/dbConnect';
 
-
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         const { ip, username, password } = req.body;
 
+        // Verifica se todos os campos estão presentes
         if (!ip || !username || !password) {
             return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
         }
@@ -21,7 +22,8 @@ export default async function handler(req, res) {
                 userId: null, // A máquina inicialmente não estará associada a nenhum usuário
             });
 
-            if (result.insertedCount > 0) {
+            // Verifica se a máquina foi adicionada com sucesso
+            if (result.acknowledged) {
                 res.status(200).json({ message: 'Máquina adicionada com sucesso' });
             } else {
                 res.status(500).json({ message: 'Erro ao adicionar máquina' });
@@ -30,6 +32,7 @@ export default async function handler(req, res) {
             res.status(500).json({ message: 'Erro interno', error: error.message });
         }
     } else {
+        // Responde com 405 para métodos não permitidos
         res.status(405).json({ message: 'Método não permitido' });
     }
 }
