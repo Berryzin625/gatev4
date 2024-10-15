f713f8302805acea707b0bc86c50a086a5353b1e
Berryzin625 <88551634+Berryzin625@users.noreply.github.com>
2024-10-15 11:58:55 -0300

    Update assign-machine.ts

--- pages/api/assign-machine.ts
+++ pages/api/assign-machine.ts
@@ -1,24 +1,24 @@
 // pages/api/assign-machine.ts
-import { connectToDatabase } from '../../lib/dbConnect';
+import dbConnect from '../../lib/dbConnect';
 import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
+    await dbConnect();
+
     if (req.method === 'POST') {
-        const { userId, machineId } = req.body;
-
-        const { db } = await connectToDatabase();
-
         try {
-            // Atualiza a máquina associando ao usuário
-            const result = await db.collection('machines').updateOne(
-                { _id: new ObjectId(machineId) },
-                { $set: { userId: new ObjectId(userId) } }
+            const { userId, machineId } = req.body;
+
+            // Lógica para associar a máquina ao usuário
+            const result = await db.collection('users').updateOne(
+                { _id: new ObjectId(userId) },
+                { $set: { machineId: machineId } }
             );
 
-            if (result.modifiedCount > 0) {
+            if (result.modifiedCount === 1) {
                 res.status(200).json({ message: 'Máquina associada com sucesso' });
             } else {
-                res.status(500).json({ message: 'Falha ao associar a máquina' });
+                res.status(404).json({ message: 'Usuário não encontrado' });
             }
         } catch (error) {
             res.status(500).json({ message: 'Erro interno', error: error.message });
